a5b15ccfb2146ce49caf33657c3a993de90ae8a0
corradocoppo <corradocoppolino@gmail.com>
2021-04-06 15:24:03 +0200

    finito

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var nome = prompt('Nome?');
+var cognome = (prompt('Cognome?'));
+var colore = (prompt('Colore preferito?'));
+var messaggioUscita = "La tua password è : " + nome + cognome + colore + "21";
+document.getElementById('password').innerHTML = messaggioUscita;
+
+
+
+
+
+
+
+
